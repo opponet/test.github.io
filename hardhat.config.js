29e0b9ea49df616b396b352abd5ee3839bfd91a0
opponent <1763273299@qq.com>
2023-03-22 16:16:46 +0800

    部署文件更新

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,7 +30,16 @@ module.exports = {
   },
   etherscan: {
     // apiKey: ETHERSCAN_API_KEY
-    apiKey: '1V9T4XBM5WP1WDTPWP48GWJ1BB4VJU5VHG'
+    apiKey: '1V9T4XBM5WP1WDTPWP48GWJ1BB4VJU5VHG',
+    // 下面的信息将用于Etherscan合约验证页面
+    // 您需要确保这些与您的合约一致
+    contractName: "Counter",
+    // 包含合约代码的Solidity文件的路径（相对于hardhat.config.js文件）
+    // 这里假设您的Solidity文件在contracts/MyContract.sol中
+    // 如果您的文件位于不同的位置，请相应地更改路径
+    sourceName: "./contracts/Counter.sol",
+    // 合约地址
+    contractAddress: "0x8281663A38E4cb7CE64d77F70a6AD208256F9Fa1"
   },
   solidity: "0.8.18",
 };
